58b70befff46be6901aa28e099fc04a26be12455
Escandon <aleskndon@hotmail.com>
2020-10-27 21:02:54 -0600

    Controladores de los modelos Permiso|Usuario|LicenciaEmpresa|

---
+++ app/Http/Controllers/LicenciaEmpresaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LicenciaEmpresa;
+use Illuminate\Http\Request;
+
+class LicenciaEmpresaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LicenciaEmpresa  $licenciaEmpresa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LicenciaEmpresa  $licenciaEmpresa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LicenciaEmpresa  $licenciaEmpresa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LicenciaEmpresa  $licenciaEmpresa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+}
