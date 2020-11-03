def4bc0b8cbc96da345eb0369c2aaa4deb60bf91
Escandon <aleskndon@hotmail.com>
2020-11-03 14:30:49 -0600

    request control the flow to corrrect data to be validated

---
+++ app/Http/Requests/LicenciaRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LicenciaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'Licencias_Emitidas' => ['required'],
+            'Empleos_Generados' => ['required'],
+            'Inversion_Generada' => ['required'],
+            'No_Asesorias' => ['required'],
+            'IdEnlaceMunicipal' => ['required'],
+            'Mes' => ['required'],
+            'Year' => ['required'],
+        ];
+    }
+}
