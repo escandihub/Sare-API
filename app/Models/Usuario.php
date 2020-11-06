01a465ad5cbcc238a6c6c6f628a3f286464af68e
Escandon <aleskndon@hotmail.com>
2020-11-05 18:58:48 -0600

    nuevos packetes no utilizados

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
 class Usuario extends Model
 {
     use HasFactory;
