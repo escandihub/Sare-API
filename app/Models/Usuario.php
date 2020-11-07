b9f3c8c5d33c007ff7f4fb36b15499111400de77
Escandon <aleskndon@hotmail.com>
2020-11-06 19:34:26 -0600

    se cambia el extends de Model a tipo Auth

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Usuario extends Authenticatable
 {
     use HasFactory;
     protected $table = 'catalogousuarios';
