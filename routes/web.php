38e4d5c7a03aace625f5b25d1e20fc990034f26e
Escandon <aleskndon@hotmail.com>
2020-11-08 18:06:47 -0600

    limpoar web

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SinglePageController;
 
-use App\Models\Usuario;
-use App\Models\Permiso;
-use App\Models\Grupo;
-use Illuminate\Support\Facades\Gate;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,12 +15,3 @@ use Illuminate\Support\Facades\Gate;
 */
 
 Route::get('/{any}',[SinglePageController::class,'index'])->where('any', '.*');
-
-Route::get('/prueba', function () {
-  $usuario = Usuario::find(1);
-// $valor =  $usuario->tienePermiso('usuario.update');
-
-Gate::authorize('tiene-acceso');
-return $usuario;
-    
-});
