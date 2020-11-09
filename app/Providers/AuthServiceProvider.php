21d5656be031283935a7ceb93bb922987c3097dd
Escandon <aleskndon@hotmail.com>
2020-11-08 18:04:54 -0600

    se regista un Gate de prueba

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\Usuario;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('tiene-acceso', function (Usuario $user, $param) {
+            return $user->tienePermiso($param);
+        });
     }
 }
