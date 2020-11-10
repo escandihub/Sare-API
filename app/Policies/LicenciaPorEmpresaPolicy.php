8c6a7a760d765fa55eec9094bad5de71d0a440e6
Escandon <aleskndon@hotmail.com>
2020-11-09 20:26:45 -0600

    politicas para el modelo licenciaEmpresaController.php

---
+++ app/Policies/LicenciaPorEmpresaPolicy.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\LicenciaEmpresa;
+use App\Models\Usuario;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+use Illuminate\Auth\Access\Response;
+
+class LicenciaPorEmpresaPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return mixed
+     */
+    public function viewAny(Usuario $usuario)
+    {
+        return $usuario->id > 0;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\usuario  $usuario
+     * @param  \App\Models\LicenciaEmpresa.php  $licenciaEmpresa.php
+     * @return mixed
+     */
+    public function view(Usuario $usuario, LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return mixed
+     */
+    public function create(Usuario $usuario)
+    {
+        return $usuario->id == 0
+        ? Response::allow()
+        : Response::deny('Prueba nadie tiene acceso ' . $usuario->nombre);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @param  \App\Models\LicenciaEmpresa.php  $licenciaEmpresa
+     * @return mixed
+     */
+    public function update(Usuario $usuario, LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\usuario  $usuario
+     * @param  \App\Models\LicenciaEmpresa.php  $licenciaEmpresa.php
+     * @return mixed
+     */
+    public function delete(Usuario $usuario, LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\usuario  $usuario
+     * @param  \App\Models\LicenciaEmpresa.php  $licenciaEmpresa.php
+     * @return mixed
+     */
+    public function restore(Usuario $usuario, LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\usuario  $usuario
+     * @param  \App\Models\LicenciaEmpresa.php  $licenciaEmpresa.php
+     * @return mixed
+     */
+    public function forceDelete(Usuario $usuario, LicenciaEmpresa $licenciaEmpresa)
+    {
+        //
+    }
+}
