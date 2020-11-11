3e5aeba45c25e1d22d73874cedb6b94c33f5639c
Escandon <aleskndon@hotmail.com>
2020-11-11 02:17:22 -0600

    trait para el event model

--- app/Models/LicenciaEmpresa.php
+++ app/Models/LicenciaEmpresa.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Traits\BicatoraEventoLogger;
+use App\Models\Traits\BitacoraModel;
+
 class LicenciaEmpresa extends Model
 {
-    use HasFactory;
+    use HasFactory, BicatoraEventoLogger;
     protected $table = 'licencias_empresa';
     protected $primaryKey = 'IdLicencia';
 
