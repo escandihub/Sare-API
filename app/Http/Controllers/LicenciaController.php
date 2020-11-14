c2da6df25c51e236a27bf4ff29792ca319c9f7de
Escandon <aleskndon@hotmail.com>
2020-11-14 15:22:25 -0600

    formated file

--- app/Http/Controllers/LicenciaController.php
+++ app/Http/Controllers/LicenciaController.php
@@ -14,104 +14,115 @@ use Illuminate\Database\Eloquent\Builder;
  */
 class LicenciaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        $validarFechas = $request->validate([
-            'fecha_inicio' => 'date',
-            'fecha_fin' => 'date',
-        ]);
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index(Request $request)
+	{
+		$validarFechas = $request->validate([
+			"fecha_inicio" => "date",
+			"fecha_fin" => "date",
+		]);
 
-         // verificar si en el params se encuntran datos 
-         $dataInicial =  !$validarFechas ? date('Y-01-01') : Date($request->fecha_inicio);
-         $dataFinal =  !$validarFechas ? date('Y-12-31') : Date($request->fecha_fin);
+		// verificar si en el params se encuntran datos
+		$dataInicial = !$validarFechas
+			? date("Y-01-01")
+			: Date($request->fecha_inicio);
+		$dataFinal = !$validarFechas ? date("Y-12-31") : Date($request->fecha_fin);
 
-         $licencia_captura = Licencia::query();
-         if(true){
-            $licencia_captura->where('IdEnlaceMunicipal', '=', 27); // Auth::user()->enlace->id
-            $licencia_captura->whereBetween("FechaCreacion", [$dataInicial, $dataFinal])->orderBy('FechaCreacion', 'desc');
-         }else{
-            $licencia_captura->whereBetween("FechaCreacion", [$dataInicial, $dataFinal])->orderBy('FechaCreacion', 'desc');
-         }
-        return response()->json($licencia_captura->paginate(10), 200);
-    }
+		$licencia_captura = Licencia::query();
+		if (true) {
+			$licencia_captura->where("IdEnlaceMunicipal", "=", 27); // Auth::user()->enlace->id
+			$licencia_captura
+				->whereBetween("FechaCreacion", [$dataInicial, $dataFinal])
+				->orderBy("FechaCreacion", "desc");
+		} else {
+			$licencia_captura
+				->whereBetween("FechaCreacion", [$dataInicial, $dataFinal])
+				->orderBy("FechaCreacion", "desc");
+		}
+		return response()->json($licencia_captura->paginate(10), 200);
+	}
 
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(LicenciaRequest $request)
+	{
+		// $request->municipio
+		// $request->month
+		$indicador = Licencia::create($request->all());
+		$indicador->IdUsuario = 5;
+		$indicador->MesConcluido = 0;
+		$indicador->save();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(LicenciaRequest $request)
-    {
-        // $request->municipio 
-        // $request->month 
-         $indicador = Licencia::create($request->all());
-         $indicador->IdUsuario = 5;
-         $indicador->MesConcluido = 0;
-         $indicador->save();
+		return response()->json(
+			["message" => "Se ha agreado un Nuevo indicador"],
+			201
+		);
+	}
 
-        return response()->json(["message" => "Se ha agreado un Nuevo indicador"], 201);
-    }
+	/**
+	 * Update the specified resource in storage.
+	 * @param route $identificador
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Models\Licencia  $licencia
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(LicenciaRequest $request, Licencia $licencia)
+	{
+		$licencia->update($request->all());
+		return response()->json(
+			[
+				"menssage" => "Actualizacion con exito",
+				"licencia" => $licencia,
+			],
+			200
+		);
+	}
 
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Models\Licencia  $licencia
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Licencia $licencia)
+	{
+		$licencia->delete();
+		return response()->json(["message" => "Se ha Eliminado"], 200);
+	}
 
-    /**
-     * Update the specified resource in storage.
-     * @param route $identificador
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Licencia  $licencia
-     * @return \Illuminate\Http\Response
-     */
-    public function update(LicenciaRequest $request, Licencia $licencia)
-    {
-        $licencia->update($request->all());
-        return response()->json([
-            'menssage' => 'Actualizacion con exito',
-            'licencia' => $licencia
-        ], 200);
-    }
+	/**
+	 * Consulta de entre dos fechas fechas
+	 * @request year-month-day
+	 * @return \Illuminate\Http\Response
+	 */
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Licencia  $licencia
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Licencia $licencia)
-    {
-        $licencia->delete();
-        return response()->json(['message' => 'Se ha Eliminado'], 200);
-    }
+	public function rangoFecha(Request $request)
+	{
+		// return response()->json($request->input('fecha_inicio'), 200);
+		$dataInicial = Date($request->fecha_inicio);
+		$dataFinal = Date($request->fecha_fin);
 
-    /**
-     * Consulta de entre dos fechas fechas 
-     * @request year-month-day
-     * @return \Illuminate\Http\Response
-     */
+		$licenciaTable = Licencia::query();
 
-     public function rangoFecha(Request $request){
-        // return response()->json($request->input('fecha_inicio'), 200);
-           $dataInicial =  Date($request->fecha_inicio);
-           $dataFinal =  Date($request->fecha_fin);
+		if ($dataInicial != null && $dataFinal != null);
+		$licenciaTable = $licenciaTable->whereBetween("FechaCreacion", [
+			$dataInicial,
+			$dataFinal,
+		]);
 
-           $licenciaTable = Licencia::query();
+		$licencias = $licenciaTable->paginate(10);
 
-           if($dataInicial != null && $dataFinal != null);
-            $licenciaTable = $licenciaTable->whereBetween('FechaCreacion',[$dataInicial, $dataFinal]);
+		return response()->json($licencias, 200);
 
-            $licencias = $licenciaTable->paginate(10);
-
-            
-        return response()->json($licencias, 200);
-
-        //    $lista = Licencia::whereBetween('FechaCreacion', [$dataInicial, $dataFinal])->paginate(10);
-        //    $lista = Licencia::paginate(10);
-
-        
-    }
+		//    $lista = Licencia::whereBetween('FechaCreacion', [$dataInicial, $dataFinal])->paginate(10);
+		//    $lista = Licencia::paginate(10);
+	}
 }
--- app/Http/Controllers/LicenciaEmpresaController.php
+++ app/Http/Controllers/LicenciaEmpresaController.php
@@ -8,6 +8,7 @@ use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
+
 /**
  * GET
  * POST
@@ -20,96 +21,107 @@ use Illuminate\Support\Facades\Auth;
  */
 class LicenciaEmpresaController extends Controller
 {
-    public function __construct()
-    {
+	public function __construct()
+	{
 		// $this->authorizeResource(LicenciaEmpresa::class);
-    }
-    /**
-     * Display a listing of the resource.
-     * @Auth  {Ususario} checa si pertenece a algun municipio
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        $validarFechas = $request->validate([
-            'fecha_inicio' => 'date',
-            'fecha_fin' => 'date',
-        ]);
-        // verificar si en el params se encuntran datos 
-        $dataInicial =  !$validarFechas ? date('Y-01-01') : Date($request->fecha_inicio);
-        $dataFinal =  !$validarFechas ? date('Y-12-31') : Date($request->fecha_fin);
+	}
+	/**
+	 * Display a listing of the resource.
+	 * @Auth  {Ususario} checa si pertenece a algun municipio
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index(Request $request)
+	{
+		$validarFechas = $request->validate([
+			"fecha_inicio" => "date",
+			"fecha_fin" => "date",
+		]);
+		// verificar si en el params se encuntran datos
+		$dataInicial = !$validarFechas
+			? date("Y-01-01")
+			: Date($request->fecha_inicio);
+		$dataFinal = !$validarFechas ? date("Y-12-31") : Date($request->fecha_fin);
 
-        $licencia_empresa = LicenciaEmpresa::query();
+		$licencia_empresa = LicenciaEmpresa::query();
 
-            // if(Auth::user()->hasMunicipio()){
-            if(false){
-                $licencia_empresa->where('IdEnlaceMunicipal', '=', 27); // Auth::user()->enlace->id
-                $licencia_empresa->whereBetween("FechaCreacion",[$dataInicial, $dataFinal]);
+		// if(Auth::user()->hasMunicipio()){
+		if (false) {
+			$licencia_empresa->where("IdEnlaceMunicipal", "=", 27); // Auth::user()->enlace->id
+			$licencia_empresa
+				->whereBetween("FechaCreacion", [$dataInicial, $dataFinal])
+				->orderBy("FechaCreacion", "desc");
+		} else {
+			//super user or admin
+			$licencia_empresa
+				->whereBetween("FechaCreacion", [$dataInicial, $dataFinal])
+				->orderBy("FechaCreacion", "desc");
+		}
 
-            }else{ //super user or admin
-                 $licencia_empresa->whereBetween("FechaCreacion",[$dataInicial, $dataFinal])->orderBy('FechaCreacion', 'desc');
-            }
-            
-        return response()->json($licencia_empresa->paginate(12), 200);
-    }
+		return response()->json($licencia_empresa->paginate(12), 200);
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(LicenciaEmpresaRequest $request)
-    {
-        $this->authorize("create");
-        $usuario = $request->IdUsuario = 5;
-        LicenciaEmpresa::create($request->all());
-        return response()->json(['message' => 'Registro agregado'], 201);
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(LicenciaEmpresaRequest $request)
+	{
+		$this->authorize("create");
+		$usuario = $request->IdUsuario = 5;
+		LicenciaEmpresa::create($request->all());
+		return response()->json(["message" => "Registro agregado"], 201);
+	}
 
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Models\LicenciaEmpresa  $licenciaEmpresa
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, LicenciaEmpresa $captura)
+	{
+		$this->authorize("update", $captura);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\LicenciaEmpresa  $licenciaEmpresa
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, LicenciaEmpresa $captura)
-    {
-        $this->authorize("update", $captura);
-        
-        $captura->update($request->all());
-        
-        return response()->json(['message' => 'Se actualizo una nueva Licencia por Empresa'], 200,);
-    }
+		$captura->update($request->all());
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\LicenciaEmpresa  $licenciaEmpresa
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(LicenciaEmpresa $captura)
-    {
-        $this->authorize("delete", $captura);
-        $captura->delete();
-        return response()->json(['message' => 'Se ha eliminado una licencia'], 200);
-    }
+		return response()->json(
+			["message" => "Se actualizo una nueva Licencia por Empresa"],
+			200
+		);
+	}
 
-    /**
-     * Consulta de entre dos fechas fechas 
-     * @request year-month-day
-     * @return \Illuminate\Http\Response
-     */
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Models\LicenciaEmpresa  $licenciaEmpresa
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(LicenciaEmpresa $captura)
+	{
+		$this->authorize("delete", $captura);
+		$captura->delete();
+		return response()->json(["message" => "Se ha eliminado una licencia"], 200);
+	}
 
-    public function porFechas(Request $request){
-    
-        $dataInicial = Date($request->fecha_inicio);
-        $dataFinal = Date($request->fecha_fin);
-        
-        $lista = LicenciaEmpresa::whereBetween('FechaCreacion', [$dataInicial, $dataFinal])->paginate(10);
+	/**
+	 * Consulta de entre dos fechas fechas
+	 * @request year-month-day
+	 * @return \Illuminate\Http\Response
+	 */
 
-        return response()->json($lista, 200);
-  }
+	public function porFechas(Request $request)
+	{
+		$dataInicial = Date($request->fecha_inicio);
+		$dataFinal = Date($request->fecha_fin);
+
+		$lista = LicenciaEmpresa::whereBetween("FechaCreacion", [
+			$dataInicial,
+			$dataFinal,
+		])->paginate(10);
+
+		return response()->json($lista, 200);
+	}
 }
