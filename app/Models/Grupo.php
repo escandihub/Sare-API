13361da270b71bdc30d84cc3b1c01ec794979c2a
Escandon <aleskndon@hotmail.com>
2020-11-16 16:09:44 -0600

    formato

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -7,56 +7,48 @@ use Illuminate\Database\Eloquent\Model;
 
 class Grupo extends Model
 {
-    use HasFactory;
-    // protected $table = 'catalogogrupos';
-    // protected $primaryKey = 'Id';
+	use HasFactory;
+	// protected $table = 'catalogogrupos';
+	// protected $primaryKey = 'Id';
 
-    // const CREATED_AT = 'FechaAlta';
-    // const UPDATED_AT = 'FechaModificacion';
-    
-    protected $fillable = [
-        'nombre',
-        'slug',
-        'descripcion',
-        'full-access'
-    ];
-    protected $hidden = [
-        
-    ];
+	// const CREATED_AT = 'FechaAlta';
+	// const UPDATED_AT = 'FechaModificacion';
 
-    public function usuario()
-    {
-        return $this->belongsToMany('App\Models\Usuario')->withTimesTamps();
-    }
-    public function permisos(){
-        return $this->belongsToMany('App\Models\Permiso')->withTimesTamps();
-    }
+	protected $fillable = ["nombre", "slug", "descripcion", "full-access"];
+	protected $hidden = [];
 
-    public function rutas()
+	public function usuario()
+	{
+		return $this->belongsToMany("App\Models\Usuario")->withTimesTamps();
+	}
+	public function permisos()
+	{
+		return $this->belongsToMany("App\Models\Permiso")->withTimesTamps();
+	}
+
+	public function rutas()
 	{
 		return $this->BelongsToMany("App\Models\menu\Ruta");
-    }
-    
-    public function getRutas()
+	}
+
+	public function getRutas()
 	{
-        $routes = [];
-        $slide = [];
-        // Grupo::find(2)->rutas
+		$routes = [];
+		$slide = [];
+		// Grupo::find(2)->rutas
 		foreach ($this->rutas as $ruta) {
-            $menu = $ruta->menu;
-            array_push($slide, $menu);
-            // return $slide;
-            if($menu["id"] == $ruta["menu_id"]){
-                array_push($routes, $ruta["route"]);
-                foreach($slide as $main){
-                if($main["id"] == $ruta["id"]){
-                 $main["submenu"] = $routes;
-                }
-            }
-            }
-            
-        }
-        return $slide;
-        
+			$menu = $ruta->menu;
+			array_push($slide, $menu);
+			// return $slide;
+			if ($menu["id"] == $ruta["menu_id"]) {
+				array_push($routes, $ruta["route"]);
+				foreach ($slide as $main) {
+					if ($main["id"] == $ruta["id"]) {
+						$main["submenu"] = $routes;
+					}
+				}
+			}
+		}
+		return $slide;
 	}
 }
