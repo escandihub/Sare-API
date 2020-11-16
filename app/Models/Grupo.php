564cf8a0712faef0317b36892b3bb6ba8f031b0c
Escandon <aleskndon@hotmail.com>
2020-11-16 16:09:14 -0600

    escribiendo metodo

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -31,4 +31,32 @@ class Grupo extends Model
     public function permisos(){
         return $this->belongsToMany('App\Models\Permiso')->withTimesTamps();
     }
+
+    public function rutas()
+	{
+		return $this->BelongsToMany("App\Models\menu\Ruta");
+    }
+    
+    public function getRutas()
+	{
+        $routes = [];
+        $slide = [];
+        // Grupo::find(2)->rutas
+		foreach ($this->rutas as $ruta) {
+            $menu = $ruta->menu;
+            array_push($slide, $menu);
+            // return $slide;
+            if($menu["id"] == $ruta["menu_id"]){
+                array_push($routes, $ruta["route"]);
+                foreach($slide as $main){
+                if($main["id"] == $ruta["id"]){
+                 $main["submenu"] = $routes;
+                }
+            }
+            }
+            
+        }
+        return $slide;
+        
+	}
 }
