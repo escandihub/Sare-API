420a01839eae58237bdc5bc83d00a0a8816a8eab
Escandon <aleskndon@hotmail.com>
2020-11-18 14:39:11 -0600

    reusing method

--- app/Http/Controllers/Reportes/CapturaReporteController.php
+++ app/Http/Controllers/Reportes/CapturaReporteController.php
@@ -4,14 +4,13 @@ namespace App\Http\Controllers\Reportes;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\LicenciaEmpresa;
 
 use PDF;
 class CapturaReporteController extends Controller
 {
 	public function documento(Type $var = null)
 	{
-		$capturas = $this->findData("2019-10-01", "2019-11-30");
+		$capturas = $this->buscarCaptura( "App\Models\LicenciaEmpresa", "2019-10-01", "2019-10-05");
 		// dd($capturas);
 		$pdf = PDF::loadView("pdfs.captura", compact("capturas"))->setPaper(
 			"letter",
@@ -19,14 +18,27 @@ class CapturaReporteController extends Controller
 		);
 		return $pdf->download("demo.pdf");
 	}
+
+	public function licencias(Type $var = null)
+	{
+		$licencias = $this->buscarCaptura("App\Models\Licencia", "2019-10-01", "2019-10-05");
+		// dd($capturas);
+		$pdf = PDF::loadView("pdfs.licencias", compact("licencias"))->setPaper(
+			"letter",
+			"landscape"
+		);
+		return $pdf->download("demo.pdf");
+	}
+
 	/**
 	 * @param  date inicio
 	 * @param  date fin
+	 * @param  Model fin
 	 * @return QueryObject
 	 */
-	public function findData($inicio, $fin)
+	public function buscarCaptura($modelo, $inicio, $fin)
 	{
-		$licencia_captura = LicenciaEmpresa::query();
+		$licencia_captura = $modelo::query();
 		$licencia_captura->where("IdEnlaceMunicipal", "=", 27); // Auth::user()->enlace->id
 		return $licencia_captura
 			->whereBetween("FechaCreacion", [$inicio, $fin])
