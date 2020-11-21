839bfc0dfd5ddb2d8debf4fc4dc57cff2f3c0fa1
Escandon <aleskndon@hotmail.com>
2020-11-20 18:48:45 -0600

    ruls to close capture work

--- app/Http/Controllers/Rules/CierreCapturaController.php
+++ app/Http/Controllers/Rules/CierreCapturaController.php
@@ -10,16 +10,18 @@ use App\Models\CierreCaptura;
 class CierreCapturaController extends Controller
 {
     /**
-     * This controller check if the country
-     * on date tu save data 
+     * This controller check if the towns
+     * Are on date to save data 
      * @api api/canCreate
+     * need specify one date plus, e.g. if today is 2020-10-01 
+     * and end day is 2020-10-01  means that cannot be possible save data
      */
 	public function index()
 	{
-		// $cierre = CierreCaptura::findOrFail(1);
+		$cierre = CierreCaptura::findOrFail(1);
 
 		$now =  new \DateTime(date("Y-m-d"));
-        $fin =  new \DateTime("2020-11-20");
+        $fin =  new \DateTime($cierre->fecha_final);
         
 		if ($fin < $now ) {
 			return response()->json(["can" => true], 200);
