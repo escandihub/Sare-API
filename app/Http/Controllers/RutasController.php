3a8be1da7a4b716a469334704961c27202eb29e1
Escandon <aleskndon@hotmail.com>
2020-11-22 20:22:56 -0600

    controlador para rutas

---
+++ app/Http/Controllers/RutasController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\menu\Ruta;
+use App\Models\menu\Menu;
+use App\Models\Grupo;
+
+class RutasController extends Controller
+{
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+        // $Menu = Ruta::has('menu')->get();
+    
+		return response()->json(Ruta::all(), 200);
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Grupo $ruta)
+	{
+        // $grupo = Grupo::find($id);
+        $grupo = $ruta;
+        $rutas = $grupo->rutas;
+        return $rutas;
+        // return response()->json($rutas, 200);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+}
