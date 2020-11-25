5ee9ee923cb86528354b9848a03085fe5101a686
Escandon <aleskndon@hotmail.com>
2020-11-25 13:10:48 -0600

    use trait to clean a controller

---
+++ app/Http/Controllers/Traits/helper.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers\Traits;
+
+trait helper
+{
+	public function replaceSpecialCharacters($municipio)
+	{
+		$cadena = str_replace(
+			["á", "é", "í", "ó", "ú"],
+			["a", "e", "i", "o", "u"],
+			$municipio
+		);
+		return $cadena;
+	}
+
+	public function convertStringToUnderscore($municipio)
+	{
+		$sin_caracteres = $this->replaceName($municipio);
+		$to_lowe_case = strtolower($sin_caracteres);
+		$string = preg_replace("/[^a-z0-9_\s-]/", "", $to_lowe_case);
+		$string = preg_replace("/[\s-]+/", " ", $string);
+		$string = preg_replace("/[\s_]/", "_", $string);
+
+		return $string;
+	}
+}
