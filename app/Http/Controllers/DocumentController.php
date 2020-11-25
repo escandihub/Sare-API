0bfe92d2b439f8165088b9fe10bbfc4107394d1d
Escandon <aleskndon@hotmail.com>
2020-11-25 13:11:10 -0600

    update methods usage

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -9,32 +9,36 @@ use Validator, Redirect, Response, File;
 use App\Http\Requests\DocumentRequest;
 
 use Illuminate\Support\Str;
+use App\Http\Controllers\Traits\helper;
 
 class DocumentController extends Controller
 {
+	use helper;
 	public function store(DocumentRequest $request)
 	{
 		// return response()->json(['archivo' =>  $request->all()], 200);
+		// return (string) Str::uuid();
+		// (string) Str::orderedUuid();
 		if ($files = $request->file("file")) {
 			$enlace_municipal = Enlace::find(21);
 
 			//Formato para el archivo a salvar
-			$sin_caracteres = $this->replaceName($enlace_municipal->Enlace_Municipal);
-			$to_lowe_case = strtolower($sin_caracteres);
-			$string = preg_replace("/[^a-z0-9_\s-]/", "", $to_lowe_case);
-			$string = preg_replace("/[\s-]+/", " ", $string);
-			$string = preg_replace("/[\s_]/", "_", $string);
-			$fecha = Date("d-m-Y");
+			$municipio = $this->convertStringToUnderscore(
+				$enlace_municipal->Enlace_Municipal
+			);
+
+			$today = Date("d-m-Y");
 
 			$nombre_archivo =
-				$string . "_" . $fecha . "." . $request->file->extension();
+				$municipio . "_" . $today . "." . $request->file->extension();
 
 			Documento::create([
 				"titulo" => $nombre_archivo,
+				"uuid" => (string) Str::orderedUuid(),
 				"municipio_id" => $enlace_municipal->id,
 			]);
 			$request->file->move(
-				public_path("uploads/" . $sin_caracteres),
+				public_path("uploads/" . $municipio),
 				$nombre_archivo
 			);
 			// $request->file->move(public_path('uploads'), '.png');
@@ -53,10 +57,8 @@ class DocumentController extends Controller
 
 	public function show(Documento $file)
 	{
-		// return (string) Str::uuid();
-		// (string) Str::orderedUuid();
-		$clean_name = $this->replaceName($file->municipio->Enlace_Municipal);
-		$path = public_path("uploads/" . $clean_name . "/" . $file->titulo);
+		return $folder_name = $this->replaceSpecialCharacters($file->municipio->Enlace_Municipal);
+		$path = public_path("uploads/" . $folder_name . "/" . $file->titulo);
 		// return $path = storage_path($file->titulo);
 
 		return Response::make(file_get_contents($path), 200, [
@@ -64,20 +66,4 @@ class DocumentController extends Controller
 			"Content-Disposition" => 'inline; filename="' . $file->titulo . '"',
 		]);
 	}
-
-	public function replaceName($municipio)
-	{
-		$cadena = str_replace(
-			["á", "é", "í", "ó", "ú"],
-			["a", "e", "i", "o", "u"],
-			$municipio
-		);
-		return $cadena;
-	}
 }
-/*
-$to_lowe_case = strtolower($enlace_municipal->Enlace_Municipal);
-            $enlace  = str_replace(" ", "_", $to_lowe_case);
-            $fecha = Date('d-m-Y');
-
-*/
