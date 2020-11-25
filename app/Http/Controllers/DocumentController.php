f67bf242427e10b2f5f3774818d61d9f4abf171f
Escandon <aleskndon@hotmail.com>
2020-11-25 08:57:37 -0600

    show method implemented

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -29,7 +29,7 @@ class DocumentController extends Controller
 
 			Documento::create([
 				"titulo" => $nombre_archivo,
-				"usuario_id" => 2,
+				"municipio_id" => $enlace_municipal->id,
 			]);
 			$request->file->move(
 				public_path("uploads/" . $sin_caracteres),
@@ -39,6 +39,26 @@ class DocumentController extends Controller
 		}
 
 		return response()->json(["message" => "Se ha creado el"], 201);
+	}
+
+	public function index()
+	{
+		$municipio = Enlace::find(21); // san cristobal
+
+		$documentos = Documento::where("municipio_id", "=", $municipio->id)->get();
+		return $documentos;
+	}
+
+	public function show(Documento $file)
+	{
+		$clean_name = $this->replaceName($file->municipio->Enlace_Municipal);
+		$path = public_path("uploads/" . $clean_name . "/" . $file->titulo);
+		// return $path = storage_path($file->titulo);
+
+		return Response::make(file_get_contents($path), 200, [
+			"Content-Type" => "application/pdf",
+			"Content-Disposition" => 'inline; filename="' . $file->titulo . '"',
+		]);
 	}
 
 	public function replaceName($municipio)
