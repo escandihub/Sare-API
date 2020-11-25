b3a165a4e8f079710ac78f8d14e3152c8261fef7
Escandon <aleskndon@hotmail.com>
2020-11-25 15:21:46 -0600

    permisos para administrar la aplicacion

--- database/seeders/PermisosSeeder.php
+++ database/seeders/PermisosSeeder.php
@@ -14,25 +14,52 @@ class PermisosSeeder extends Seeder
 	 */
 	public function run()
 	{
-		Permiso::create([
-			'nombre' => '',
-			'slug' => 'usuario.index',
-			'descripcion' => '',
-		]);   
-		Permiso::create([
-			'nombre' => '',
-			'slug' => 'usuario.update',
-			'descripcion' => '',
-		]);   
-		Permiso::create([
-			'nombre' => '',
-			'slug' => 'captura.index',
-			'descripcion' => '',
-		]);   
-		Permiso::create([
-			'nombre' => '',
-			'slug' => 'captura.own.index',
-			'descripcion' => '',
-		]);   
+		// Permiso::create([
+		// 	'nombre' => '',
+		// 	'slug' => 'usuario.index',
+		// 	'descripcion' => '',
+		// ]);   
+		// Permiso::create([
+		// 	'nombre' => '',
+		// 	'slug' => 'usuario.update',
+		// 	'descripcion' => '',
+		// ]);   
+		// Permiso::create([
+		// 	'nombre' => '',
+		// 	'slug' => 'captura.index',
+		// 	'descripcion' => '',
+		// ]);   
+		// Permiso::create([
+		// 	'nombre' => '',
+		// 	'slug' => 'captura.own.index',
+		// 	'descripcion' => '',
+		// ]);   
+
+		// Permisos para subir archivos o ver archivos subidos 
+		// Permiso::create([
+		// 	'nombre' => 'puede generar archivo',
+		// 	'slug' => 'documento.generate',
+		// 	'descripcion' => 'Generar archivo que ha guardado en captura por empresa',
+		// ]); 
+		// Permiso::create([
+		// 	'nombre' => 'puede subir archivo',
+		// 	'slug' => 'documento.store',
+		// 	'descripcion' => 'Puede subir documentos firmados',
+		// ]); 
+		// Permiso::create([
+		// 	'nombre' => 'puede ver sus propios archivos',
+		// 	'slug' => 'documento.own.show',
+		// 	'descripcion' => 'Puede ver sus archivos que el usuario ha subido',
+		// ]);
+		// Permiso::create([
+		// 	'nombre' => 'puede ver sus todos los archivos',
+		// 	'slug' => 'documento.show',
+		// 	'descripcion' => 'Puede ver todos los archivos',
+		// ]);
+		// Permiso::create([
+		// 	'nombre' => 'puede actualizar sus propios archivos',
+		// 	'slug' => 'documento.own.update',
+		// 	'descripcion' => 'Puede actualizar sus archivos que el usuario ha subido',
+		// ]);
 	}
 }
