60a6fd6d649a2982b15c12345c90f9c14007b087
Escandon <aleskndon@hotmail.com>
2020-11-26 10:31:43 -0600

    formated and where clausule

--- app/Http/Controllers/Rules/ConcluirMesEmpresaController.php
+++ app/Http/Controllers/Rules/ConcluirMesEmpresaController.php
@@ -9,67 +9,77 @@ use App\Models\LicenciaEmpresa;
 
 class ConcluirMesEmpresaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-         $month = \Date('m');
-          $year = \Date('Y');
-        return $empresas = LicenciaEmpresa::whereRaw("MONTH(FechaCreacion) = ? AND YEAR(FechaCreacion) = ?", [$month, $year])->with("municipio")->selectRaw("DISTINCT licencias_empresa.IdEnlaceMunicipal, licencias_empresa.MesConcluido")->orderBy("IdEnlaceMunicipal", "DESC")->get();
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$month = \Date("m");
+		$year = \Date("Y");
+		return $empresas = LicenciaEmpresa::whereRaw(
+			"MONTH(FechaCreacion) = ? AND YEAR(FechaCreacion) = ?",
+			[$month, $year]
+		)
+			->with("municipio")
+			->selectRaw(
+				"DISTINCT licencias_empresa.IdEnlaceMunicipal, licencias_empresa.MesConcluido"
+			)
+			->orderBy("IdEnlaceMunicipal", "DESC")
+			->get();
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $month = \Date('m');
-        $year = \Date('Y');
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$month = \Date("m");
+		$year = \Date("Y");
 
-        foreach($request->all() as $key => $value) {
-           LicenciaEmpresa::where("IdEnlaceMunicipal", "=", $value['IdEnlaceMunicipal'])->update(["MesConcluido" => $value['MesConcluido']]);
-        }
-        return response()->json(["message" => "OK"], 200);
-        
-    }
+		foreach ($request->all() as $key => $value) {
+			LicenciaEmpresa::where("IdEnlaceMunicipal", $value["IdEnlaceMunicipal"])
+				->where("Year", $year)
+				->where("Mes", $month)
+				->update(["MesConcluido" => $value["MesConcluido"]]);
+		}
+		return response()->json(["message" => "OK"], 200);
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
