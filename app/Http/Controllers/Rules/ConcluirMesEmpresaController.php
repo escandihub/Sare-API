b0b48e27c8793111977b787d61d3c2de4a910f03
Escandon <aleskndon@hotmail.com>
2020-11-26 08:59:08 -0600

    index lista de capturas de empresa para dar a conluir

---
+++ app/Http/Controllers/Rules/ConcluirMesEmpresaController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Rules;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\LicenciaEmpresa;
+
+class ConcluirMesEmpresaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         $month = \Date('m');
+          $year = \Date('Y');
+        return $empresas = LicenciaEmpresa::whereRaw("MONTH(FechaCreacion) = ? AND YEAR(FechaCreacion) = ?", [$month, $year])->with("municipio")->selectRaw("DISTINCT licencias_empresa.IdEnlaceMunicipal, licencias_empresa.MesConcluido")->orderBy("IdEnlaceMunicipal", "DESC")->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
