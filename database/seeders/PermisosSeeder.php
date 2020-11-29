896fde7c440be99e49c0141e599cdd133b7d44c9
Escandon <aleskndon@hotmail.com>
2020-11-29 17:48:22 -0600

    permisos para aliminetar a la db

--- database/seeders/PermisosSeeder.php
+++ database/seeders/PermisosSeeder.php
@@ -14,52 +14,123 @@ class PermisosSeeder extends Seeder
 	 */
 	public function run()
 	{
-		// Permiso::create([
-		// 	'nombre' => '',
-		// 	'slug' => 'usuario.index',
-		// 	'descripcion' => '',
-		// ]);   
-		// Permiso::create([
-		// 	'nombre' => '',
-		// 	'slug' => 'usuario.update',
-		// 	'descripcion' => '',
-		// ]);   
-		// Permiso::create([
-		// 	'nombre' => '',
-		// 	'slug' => 'captura.index',
-		// 	'descripcion' => '',
-		// ]);   
-		// Permiso::create([
-		// 	'nombre' => '',
-		// 	'slug' => 'captura.own.index',
-		// 	'descripcion' => '',
-		// ]);   
-
-		// Permisos para subir archivos o ver archivos subidos 
-		// Permiso::create([
-		// 	'nombre' => 'puede generar archivo',
-		// 	'slug' => 'documento.generate',
-		// 	'descripcion' => 'Generar archivo que ha guardado en captura por empresa',
-		// ]); 
-		// Permiso::create([
-		// 	'nombre' => 'puede subir archivo',
-		// 	'slug' => 'documento.store',
-		// 	'descripcion' => 'Puede subir documentos firmados',
-		// ]); 
-		// Permiso::create([
-		// 	'nombre' => 'puede ver sus propios archivos',
-		// 	'slug' => 'documento.own.show',
-		// 	'descripcion' => 'Puede ver sus archivos que el usuario ha subido',
-		// ]);
-		// Permiso::create([
-		// 	'nombre' => 'puede ver sus todos los archivos',
-		// 	'slug' => 'documento.show',
-		// 	'descripcion' => 'Puede ver todos los archivos',
-		// ]);
-		// Permiso::create([
-		// 	'nombre' => 'puede actualizar sus propios archivos',
-		// 	'slug' => 'documento.own.update',
-		// 	'descripcion' => 'Puede actualizar sus archivos que el usuario ha subido',
-		// ]);
+		//administracion de usuarios
+		Permiso::create([
+			"nombre" => "",
+			"slug" => "usuario.index",
+			"descripcion" => "",
+		]);
+		Permiso::create([
+			"nombre" => "",
+			"slug" => "usuario.update",
+			"descripcion" => "puede actualizar a cualquier usuario",
+		]);
+		Permiso::create([
+			"nombre" => "",
+			"slug" => "usuario.delete",
+			"descripcion" => "Puede eliminar a cualquier usuario",
+		]);
+		//grupos y privilegios
+		Permiso::create([
+			"nombre" => "grupos y privilegios",
+			"slug" => "grupo.index",
+			"descripcion" => "Puede ver los grupos",
+		]);
+		Permiso::create([
+			"nombre" => "grupos y privilegios",
+			"slug" => "grupo.update",
+			"descripcion" => "Puede actualizar los privilegios del los grupos",
+		]);
+		Permiso::create([
+			"nombre" => "grupos y privilegios",
+			"slug" => "grupo.delete",
+			"descripcion" => "Puede eliminar los grupos",
+		]);
+		//rutas de acceso
+		Permiso::create([
+			"nombre" => "Acceso a rutas",
+			"slug" => "rutas.index",
+			"descripcion" => "Puede ver la lista de rutas",
+		]);
+		Permiso::create([
+			"nombre" => "Acceso a rutas",
+			"slug" => "rutas.show",
+			"descripcion" => "Puede ver la lista de rutas de cada grupo",
+		]);
+		Permiso::create([
+			"nombre" => "Acceso a rutas",
+			"slug" => "rutas.update",
+			"descripcion" => "Puede actualizar las rutas de los grupos",
+		]);
+		//Bitacora 
+		Permiso::create([
+			"nombre" => "Bitacora",
+			"slug" => "bitacora.index",
+			"descripcion" => "Puede ver la lista de bitacoras",
+		]);
+		// Captura por empresa
+		Permiso::create([
+			"nombre" => "Captura por empresa",
+			"slug" => "captura.index",
+			"descripcion" => "Puede ver las lista de las capturas",
+		]);
+		Permiso::create([
+			"nombre" => "Captura por empresa",
+			"slug" => "captura.update",
+			"descripcion" => "puede editar las capturas",
+		]);
+		Permiso::create([
+			"nombre" => "Captura por empresa",
+			"slug" => "captura.delete",
+			"descripcion" => "puede eliminar las capturas",
+		]);
+		// Indicador general
+		Permiso::create([
+			"nombre" => "Indicador general",
+			"slug" => "indicador.index",
+			"descripcion" => "Puede ver las lista de los indicadores",
+		]);
+		Permiso::create([
+			"nombre" => "indicador general",
+			"slug" => "indicador.update",
+			"descripcion" => "puede editar las indicadores",
+		]);
+		Permiso::create([
+			"nombre" => "indicador general",
+			"slug" => "indicador.delete",
+			"descripcion" => "puede eliminar los indicadores",
+		]);
+		// Permisos para subir archivos o ver archivos subidos
+		Permiso::create([
+			"nombre" => "Gestion de documentos",
+			"slug" => "documento.generate",
+			"descripcion" => "Generar archivo que ha guardado en captura por empresa",
+		]);
+		Permiso::create([
+			"nombre" => "Gestion de documentos",
+			"slug" => "documento.store",
+			"descripcion" => "Puede subir documentos firmados",
+		]);
+		Permiso::create([
+			"nombre" => "Gestion de documentos",
+			"slug" => "documento.own.show",
+			"descripcion" => "Puede ver sus archivos que el usuario ha subido",
+		]);
+		Permiso::create([
+			"nombre" => "Gestion de documentos",
+			"slug" => "documento.show",
+			"descripcion" => "Puede ver todos los archivos subidos",
+		]);
+		Permiso::create([
+			"nombre" => "Gestion de documentos",
+			"slug" => "documento.own.update",
+			"descripcion" => "Puede actualizar sus archivos que el usuario ha subido",
+		]);
 	}
 }
+
+// Permiso::create([
+// 	"nombre" => "",
+// 	"slug" => "usuario.own.update",
+// 	"descripcion" => "Puede actualizar su propio usuario",
+// ]);
