ad525916f3c581aeee5a09a47edc7b65a0db3138
Escandon <aleskndon@hotmail.com>
2020-12-15 18:32:46 -0600

    test deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,20 +10,38 @@
     <link rel="icon" href="{{asset('dist/favicon.ico') }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>sare-front</title>
+    <title>  {{ env('APP_NAME') }}   </title>
 
-<link href="{{ asset('/dist/js/about.e5d86d95.js')}}" rel="prefetch" />
-<link href="{{ asset('/dist/js/chunk-02b643a9.c5bf3eea.js')}}" rel="prefetch" />
-<link href="{{ asset('/dist/js/chunk-2d0a3c8d.5d457f1a.js')}}" rel="prefetch" />
-<link href="{{ asset('/dist/js/chunk-2d0b721e.01c7ebd2.js')}}" rel="prefetch" />
-<link href="{{ asset('/dist/js/chunk-2d216214.926a9779.js')}}" rel="prefetch" />
-<link href="{{ asset('/dist/js/chunk-2d216257.256ee085.js')}}" rel="prefetch" />
-<link href="{{ asset('/dist/js/chunk-2d2244c4.000716c4.js')}}" rel="prefetch" />
-<link href="{{ asset('/dist/css/app.22f826e1.css')}}" rel="preload" as="style" />
-<link href="{{ asset('/dist/js/app.d0661bf8.js')}}" rel="preload')}}" as="script" />
-<link href="{{ asset('/dist/js/chunk-vendors.5c99db81.js')}}" rel="preload" as="script" />
-<link href="{{ asset('/dist/css/app.22f826e1.css')}}" rel="stylesheet" />
-
+    <script>
+      window.config = {
+        config: {
+          URL_API: 'http://32800abb0ca7.ngrok.io'
+        }
+      }
+    </script>
+		<link href="{{ asset('/dist/css/chunk-210452ed.9b2c5b6b.css') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/css/chunk-5d09c4e2.26fb9dcc.css') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/about.e5d86d95.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-210452ed.62bce3c7.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d0b1d7b.da9427d4.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d0b2d57.df35902f.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d0c061b.782c3d81.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d0d2b06.f6857163.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d0d6909.3210c090.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d216214.926a9779.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d216257.256ee085.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d217abc.22876c62.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-2d2244c4.8ac469b9.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-33a72d9b.96c30720.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-354a9ca9.fb404b17.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-3c126d84.0fe1726f.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-5d09c4e2.50df2277.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-7c2a37e4.a6b9c081.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/js/chunk-9e9c9b90.5c5cdf13.js') }}" rel="prefetch" />
+		<link href="{{ asset('/dist/css/app.96098bc6.css') }}" rel="preload" as="style" />
+		<link href="{{ asset('/dist/js/app.5c352651.js') }}" rel="preload" as="script" />
+		<link href="{{ asset('/dist/js/chunk-vendors.46955cb0.js') }}" rel="preload" as="script" />
+		<link href="{{ asset('/dist/css/app.96098bc6.css') }}" rel="stylesheet" />
 
 
 
@@ -44,7 +62,7 @@
       ></noscript
     >
     <div id="app"></div>
-    <script src="{{ asset('/dist/js/chunk-vendors.5c99db81.js')}}"></script>
-    <script src="{{ asset('/dist/js/app.d0661bf8.js')}}"></script>
+    <script src="{{ asset('/dist/js/chunk-vendors.46955cb0.js')}}"></script>
+    <script src="{{ asset('/dist/js/app.5c352651.js')}}"></script>
   </body>
 </html>
